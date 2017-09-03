98024384a54db0b6f8ecc7e2426c56d8992597b2
Andrew McMechan <coderwurst@users.noreply.github.com>
2017-09-03 13:44:15 +0200

    Kapital 4.4 Fallunterscheidungen

--- jsx/src/main.js
+++ jsx/src/main.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Title(props) {
-  return <h1>{props.label}</h1>;
+  return <h1>{props.label} {props.name}</h1>;
 }
 function Header() {
-  return <div className='header'><Title label='Hello, World' /></div>;
+  return <div className='header'><Title label='Hello, ' name = 'Jimmy McGill' /></div>;
+}
+function ErrorMessage() {
+  return <div> { isError() ? <h4>an error has occurred, please refresh the page and try again</h4> : null } </div>;
+}
+
+function isError() {
+  return getRandom() > 5;
+}
+
+function getRandom() {
+  return Math.random() * (10 - 1) + 1;
 }
 
 function Danger() {
@@ -23,6 +34,7 @@ function Page() {
   return <div>
     <Header />
     <Danger />
+    <ErrorMessage />
   </div>
 }
 
